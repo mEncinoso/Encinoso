92090f2bd2e799b504f59870d8122409fc51f896
Mati <jose.encinososanchez@gmail.com>
2020-12-23 19:29:39 +0000

    2x1 Websites adapted
    - 2 Websites operative
    - Ferre maps
    - Ferre cover

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -1,4 +1,8 @@
-document.getElementById("encinoso-logo").addEventListener("click", function () {
+document.getElementById("expo-encinoso-logo").addEventListener("click", function () {
+  showContent("webContent", "indexContent");
+});
+
+document.getElementById("ferre-encinoso-logo").addEventListener("click", function () {
   showContent("webContent", "indexContent");
 });
 
@@ -6,20 +10,33 @@ document.getElementById("btn-toExpo").addEventListener("click", function () {
   showContent("webContent", "expo");
 });
 
+document.getElementById("expo_initialContentLink").addEventListener("click", function () {
+    showContent("expo_content", "expo_init_content");
+});
+
+document.getElementById("expo_locationLink").addEventListener("click", function () {
+  showContent("expo_content", "expo_location_content");
+});
+
+document.getElementById("expo_contact_link").addEventListener("click", function () {
+  showContent("expo_content", "expo_contact_content");
+});
+
+
 document.getElementById("btn-toFerre").addEventListener("click", function () {
   showContent("webContent", "ferre");
 });
 
-document.getElementById("initialContentLink").addEventListener("click", function () {
-    showContent("content", "init_content");
+document.getElementById("ferre_initialContentLink").addEventListener("click", function () {
+  showContent("ferre_content", "ferre_init_content");
 });
 
-document.getElementById("locationLink").addEventListener("click", function () {
-  showContent("content", "location_content");
+document.getElementById("ferre_locationLink").addEventListener("click", function () {
+showContent("ferre_content", "ferre_location_content");
 });
 
-document.getElementById("contact_link").addEventListener("click", function () {
-  showContent("content", "contact_content");
+document.getElementById("ferre_contact_link").addEventListener("click", function () {
+showContent("ferre_content", "ferre_contact_content");
 });
 
 function showContent(container, content) {
@@ -27,13 +44,13 @@ function showContent(container, content) {
   for (var i = 0; i < children.length; i++) {
     if (children[i].id != content) {
       document.getElementById(children[i].id).style.display = "none";
-      if (container == "content") {
+      if (container == "content" || container == "ferre_content") {
         document.getElementById(children[i].id).style.visibility = "hidden";
         document.getElementById(children[i].id).style.opacity = 0;
       }
     } else {
       document.getElementById(children[i].id).style.display = "block";
-      if (container == "content") {
+      if (container == "expo_content" || container == "ferre_content") {
         document.getElementById(children[i].id).style.visibility = "visible";
         document.getElementById(children[i].id).style.opacity = 1;
       }
--- src/js/main.js
+++ src/js/main.js
@@ -12,34 +12,18 @@ $(window).on("resize", function () {
     setPaddingTop("expo");
 });
 
+document.getElementById("btn-toFerre").addEventListener("click", function(){
+  setPaddingTop("ferre");
+});
+
+$(window).on("resize", function () {
+    setPaddingTop("ferre");
+});
+
 function setPaddingTop(id){
-  if(id=="expo"){
+  if(id=="expo" || id=="ferre"){
     var content_padding_top;
-    content_padding_top = $("header").height();
-    $("#carousel").css("padding-top", content_padding_top);
+    content_padding_top = $("#"+id+"_header").height();
+    $("#"+id+"_carousel").css("padding-top", content_padding_top);
   }
 }
-
-/*$(document).scroll(function() {
-    $('#header').css({width: $(this).scrollTop() > 100? "50%":"100%"});
-});*/
-
-//Resize nav-bar size when scroll is not at top
-/*var scroll;
-var upper_height = 10;
-var lower_height = 5;
-
-$(window).on("scroll", function () {
-  scroll = $(window).scrollTop();
-  if (scroll > 0) {
-    $("#header").css("height", lower_height + "%");
-  } else {
-    $("#header").css("height", upper_height + "%");
-  }
-  setPaddingTop();
-});*/
-
-
-
-
-
