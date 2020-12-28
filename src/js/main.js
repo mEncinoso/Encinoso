b207e6e3fd089a3617135077533bcee57ff7b1b6
Mati <jose.encinososanchez@gmail.com>
2020-12-28 09:03:38 +0000

    Ferre Adapting
    CxM Date Resume

--- src/js/main.js
+++ src/js/main.js
@@ -4,26 +4,26 @@ import "./listeners";
 import "./bootstrap-props.js";
 
 //Change content margin-top depending on nav-bar header height (on load and rezising)
-document.getElementById("btn-toExpo").addEventListener("click", function(){
+document.getElementById("btn-toExpo").addEventListener("click", function () {
   setPaddingTop("expo");
 });
 
 $(window).on("resize", function () {
-    setPaddingTop("expo");
+  setPaddingTop("expo");
 });
 
-document.getElementById("btn-toFerre").addEventListener("click", function(){
+document.getElementById("btn-toFerre").addEventListener("click", function () {
   setPaddingTop("ferre");
 });
 
 $(window).on("resize", function () {
-    setPaddingTop("ferre");
+  setPaddingTop("ferre");
 });
 
-function setPaddingTop(id){
-  if(id=="expo" || id=="ferre"){
+function setPaddingTop(id) {
+  if (id == "expo" || id == "ferre") {
     var content_padding_top;
-    content_padding_top = $("#"+id+"_header").height();
-    $("#"+id+"_carousel").css("padding-top", content_padding_top);
+    content_padding_top = $("#" + id + "_header").height();
+    $("#" + id + "_carousel").css("padding-top", content_padding_top);
   }
 }
